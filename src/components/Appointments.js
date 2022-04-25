61865a82d60ca73cb6c0d417706ec94a0978918c
Kevin~ <kevingeomathew@gmail.com>
2022-04-25 20:41:06 +0530

    adding new files

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,9 +1,89 @@
+import { useHistory } from "react-router-dom"
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import App from "../App"
+import React, { useState, useEffect } from "react";
+import {auth, db} from "../FireBase";
+
+var symptom1 ="", symptom2="", intensity1="", intensity2="", addInfo="", days=""
+var user = ""
+var picURL = ""
 
 const Appointments = () => {
+    let history = useHistory()
+
+
+    const [users, setUsers] = useState([])
+    const usersCollectionRef = collection(db, "appointments")
+
+    if(auth.currentUser)
+     {
+         user = auth.currentUser.uid
+         //console.log("appoitnemnts", user)
+         //console.log("to check url for appointments",auth.currentUser)
+         picURL = auth.currentUser.photoURL
+     }
+
+
+
+     useEffect(() => {
+
+        const getUsers = async () => {
+            //creating a reference to the collection we want
+        
+        const data = await getDocs(usersCollectionRef)//returns all document from a collection
+        if(data){
+            setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+            //console.log("inside try")
+            //console.log("users console", users)
+        }    
+        }
+        getUsers()
+    }, []);
+
+    users.map( (u) =>{
+        if(u.userid == user)
+        {
+            //console.log("inside the map",u)
+            symptom1 = u.symptom1 
+            symptom2= u.symptom2 
+            intensity1= u.intensity1 
+            intensity2= u.intensity2 
+            addInfo= u.addinfo
+            days= u.days
+            //console.log(symptom1)
+        }
+    } )
+
+
     return(
         <div>
-            Space for Appointments
+            
+            <button style={{float: "left"}} onClick={() => { history.push('home') }}>Back</button>
+
+            <div>
+                <img src={picURL} style={{"borderRadius": "50%", "width":"15%", "height":"15%"}}></img><br/>
+            </div>
+            <label>Symptom 1</label>
+            <input placeholder="symptom1" value={symptom1} style={{width:"500px", "display":"inline-block"}} /><br/><br />
+            
+            <label>Intensity</label>
+            <input placeholder="intensity"  value={intensity1} style={{width:"500px",  "display":"inline-block"}} /><br /><br />
+
+            <label>Symptom 2</label>
+            <input placeholder="symptom2" value={symptom2} style={{width:"500px", "display":"inline-block"}} /><br /><br />
+            
+
+            <label>Intensity 2</label>
+            <input placeholder="intensity2" type="email" value={intensity2} style={{width:"500px", "display":"inline-block"}} /><br /><br />
+
+            <label>Days</label>
+            <input placeholder="Days" value={days} style={{width:"500px", "display":"inline-block"}}  /><br /><br />
+
+            <label>Additional Info</label>
+            <input placeholder="addinfo" value={addInfo} style={{width:"500px", "display":"inline-block"}} /><br /><br />
+
+            
+
         </div>
     )
 }
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,52 +1,60 @@
 //import { async } from "@firebase/util";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { auth, db, signedInUserEmail, userData } from "../FireBase";
 import ProfileFields from "./ProfileFields";
 import './UserProfile.css'
 
 
+
 var currFirstName ="", currLastname ="", currAge = "", currEmail ="", currHeight = 0, currWeight = 0, currAllergies = "", currChronic = "";
+var user = ""
+var picURL = ""
 
 const UserProfile = () =>{
-
-     const [users, setUsers] = useState([])
-     const usersCollectionRef = collection(db, "userprofile")
-
+    let history = useHistory()
+    const [users, setUsers] = useState([])
+    const usersCollectionRef = collection(db, "users")
+     
+     if(auth.currentUser)
+     {
+         user = auth.currentUser.uid
+         //console.log(user)
+         //console.log("to check url",auth.currentUser)
+         picURL = auth.currentUser.photoURL
+     }
      
     //we need to query the firestore now, as soon as the component renders and hence, we'll use useEffect
     useEffect(() => {
 
         const getUsers = async () => {
             //creating a reference to the collection we want
-            try{
-            const data = await getDocs(usersCollectionRef)//returns all document from a collection
+        
+        const data = await getDocs(usersCollectionRef)//returns all document from a collection
+        if(data){
             setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-            setTimeout(5000)
-            console.log("inside try")
-            }
-            finally{
-                console.log("inside finally")
-                console.log(users.firstname)
-
-            }
+            //console.log("inside try")
+            //console.log("users console", users)
+        }    
         }
-
         getUsers()
-
-        //setTimeout(()=>{console.log("SetTime out ->", users.firstname)}, 8000)
     }, []);
 
-    
-
-    //State for the user variables
-    const [newName, setNewName] = useState("")
-    const [newLast, setLastName] = useState("")
-    const [newAge, setNewAge] = useState(0)
-
-    const createUser = async () => {//we gotta reference our collection from db
-        await addDoc(usersCollectionRef, {firstname: newName, lastname: newLast, age: newAge } ) 
-    }
+    users.map( (u) =>{
+        if(u.userid == user)
+        {
+            console.log("inside the map",u)
+            currFirstName = u.firstname
+            currLastname = u.lastname
+            currAge = u.age
+            currEmail = u.email
+            currHeight = u.height
+            currWeight = u.weight
+            currAllergies = u.allergies
+            currChronic = u.chronic
+        }
+    } )
 
     
 
@@ -54,43 +62,32 @@ const UserProfile = () =>{
 
     return(
         <div>
-            <h2>This is user's profile</h2>
-            
-            {users.map((user) => 
-            {
-                return <div>
-                            <h2>FirstName: {user.firstname}</h2>
-                            <h2>LastName : {user.lastname}</h2>
-                        </div> 
-            })
-            }
-                    
-
+            <button style={{float: "left"}} onClick={() => { history.push('home') }}>Back</button><br /><br /><br /><br />
+            <div>
+                <img src={picURL} style={{"borderRadius": "50%", "width":"15%", "height":"15%"}}></img><br/>
+            </div>
             <label>First Name</label>
-            <input placeholder="First Name" value={currFirstName} onChange={(event) => { setNewName(event.target.value) }} /><br/>
+            <input placeholder="First Name" value={currFirstName} style={{width:"500px"}} /><br/><br />
             <label>Last Name</label>
-            <input placeholder="Last Name" value={currLastname} onChange={(event) => { setLastName(event.target.value) }} /><br />
+            <input placeholder="Last Name" value={currLastname} style={{width:"500px"}} /><br /><br />
             <label>Age</label>
-            <input placeholder="Age" type="number" value={currAge} onChange={(event) => { setNewAge(event.target.value) }} /><br />
+            <input placeholder="Age" type="number" value={currAge} style={{width:"500px"}} /><br /><br />
 
             <label>Email</label>
-            <input placeholder="Email" type="email" value={currEmail} /><br />
+            <input placeholder="Email" type="email" value={currEmail} style={{width:"500px"}} /><br /><br />
 
             <label>Height</label>
-            <input placeholder="Height" value={currHeight} /><br />
+            <input placeholder="Height" value={currHeight} style={{width:"500px"}}  /><br /><br />
 
             <label>Weight</label>
-            <input placeholder="Weight" value={currWeight} /><br />
+            <input placeholder="Weight" value={currWeight} style={{width:"500px"}} /><br /><br />
 
             <label>Allergies</label>
-            <input placeholder="Allergies" value={currAllergies} /><br />
+            <input placeholder="Allergies" value={currAllergies} style={{width:"500px"}} /><br /><br />
 
             <label>Chronic Diseases</label>
-            <input placeholder="Chronic Diseases" value={currChronic} /><br />
+            <input placeholder="Chronic Diseases" value={currChronic} style={{width:"500px"}} /><br />
 
-            <button onClick={createUser}>Create</button>
-            
-            
             
 
         </div>
